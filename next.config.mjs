50015ded7980727cecf0cf5faf881a162b42e3ee
b0b0aung <160343524+boboaung1337@users.noreply.github.com>
2026-08-31 05:50:21 +0000

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
+}
+
+export default nextConfig
